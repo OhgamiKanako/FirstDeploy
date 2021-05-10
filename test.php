46b50d65ac24f001d8ba6e30e5e636eb9fe48534
OhgamiKanako <74999846+OhgamiKanako@users.noreply.github.com>
2021-05-10 10:51:40 +0900

    update

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
- <link rel="styleseet" href="/style.css">
+ <link rel="styleseet" href="https://OhgamiKanako.github.io/FirstDeploy/style.css">
 </head>
 <body>
  <h1>test Success!!</h1>
