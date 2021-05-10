814054a251ffed27c34a2078bca04b11533d6457
OhgamiKanako <74999846+OhgamiKanako@users.noreply.github.com>
2021-05-10 10:37:09 +0900

    update

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
- <link rel="styleseet" href="/style.css">
+ <link href="/style.css">
 </head>
 <body>
  <h1>Pipeline & Deploy Success!!</h1>
  <h2>AWS完了</h2>
- <link rel="test" href="/test.php">
+ <a rel="test" href="test.php">
  <?php phpinfo() ?>
 </body>
 </html>
