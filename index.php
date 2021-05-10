84a8e6188467ef8cb6d18f10c1d3f0772bc8d066
OhgamiKanako <74999846+OhgamiKanako@users.noreply.github.com>
2021-05-10 10:30:49 +0900

    update

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
- <link rel="styleseet" href="https://github.com/OhgamiKanako/FirstDeploy/blob/main/style.css">
+ <link rel="styleseet" href="/style.css">
 </head>
 <body>
  <h1>Pipeline & Deploy Success!!</h1>
  <h2>AWS完了</h2>
+ <link rel="test" href="/test.php">
  <?php phpinfo() ?>
 </body>
 </html>
