2c790b0188c8212cb0c9a7ffc05751e2f1c1d9f4
OhgamiKanako <74999846+OhgamiKanako@users.noreply.github.com>
2021-05-10 10:48:06 +0900

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
- <link rel="stylesheet" href="https://OhgamiKanako.github.io/FirstDeploy/style.css"/>
+ <link rel="stylesheet" href="https://OhgamiKanako.github.io/FirstDeploy/style.css">
 </head>
 <body>
  <h1>Pipeline & Deploy Success!!</h1>
